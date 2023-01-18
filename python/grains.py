a526131639ffdbaf76cf2e1b2cdfbd006215b5c6
Raga <ragawilliam570@gmail.com>
2023-01-18 14:02:19 +0300

    Add grains solution

---
+++ python/grains.py
@@ -0,0 +1,30 @@
+def create_arr():
+  arr = [1, ]
+  for i in range(1, 65):
+    num = arr[-1] * 2
+    # print("{} * 2: {}".format(i, num))
+    arr.append(num)
+  return arr
+
+def square(number):
+  if number >= 1 and number <= 64:
+    square_arr = create_arr()
+    # print(square_arr[number - 1])
+    return square_arr[number - 1]
+
+  else:
+    raise ValueError("square must be between 1 and 64")
+
+
+def total():
+    sum = 0
+    for i in create_arr():
+      sum += i
+
+    return sum
+
+print(square(3))
+print(total())
+
+
+# print(create_arr())
