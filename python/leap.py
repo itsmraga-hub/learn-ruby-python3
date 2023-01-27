7e29678b6c3ce9cd3727301b478bd67b6d72447d
Raga <ragawilliam570@gmail.com>
2023-01-27 22:34:12 +0300

    Add leap partial solution

---
+++ python/leap.py
@@ -0,0 +1,11 @@
+def leap_year(year):
+  if year % 400 == 0 and year % 4 == 0:
+    return True
+  elif year % 100 != 0 and year % 4 == 0:
+    return True
+  elif year % 4 == 0:
+    return True
+  else:
+    return False
+
+print(leap_year(2100))
