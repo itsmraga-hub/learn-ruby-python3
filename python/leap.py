7078a63ac2bf72d4a9000f1d43c5b1c58a5522dc
Raga <ragawilliam570@gmail.com>
2023-01-28 23:35:12 +0300

    Partial solution

--- python/leap.py
+++ python/leap.py
@@ -1,11 +1,17 @@
 def leap_year(year):
-  if year % 400 == 0 and year % 4 == 0:
+  """
+    on every year that is evenly divisible by 4
+    except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+  """
+  # mod_four = year % 4
+  if year % 4 == 0:
     return True
-  elif year % 100 != 0 and year % 4 == 0:
-    return True
-  elif year % 4 == 0:
-    return True
+  elif year % 100 == 0:
+    if year % 400 == 0:
+      return True
+    return False
   else:
     return False
 
-# print(leap_year(2100))
+print(leap_year(2100))
