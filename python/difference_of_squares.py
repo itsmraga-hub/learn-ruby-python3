aee31851ef4ddffd38f7864afc787e49aa7fae05
Raga <ragawilliam570@gmail.com>
2023-01-29 19:27:03 +0300

    Add difference soln

---
+++ python/difference_of_squares.py
@@ -0,0 +1,18 @@
+def square_of_sum(number):
+  sum_square = 0
+  for i in number:
+    sum_square += i
+
+
+
+def sum_of_squares(number):
+  sum_squares = 0
+  for i in number:
+    sq = i * i
+    sum_squares += sq
+  
+
+
+
+def difference_of_squares(number):
+    pass
