8a4e7dc0a1301ab5b480da4e536483ba4b85ddd2
Raga <ragawilliam570@gmail.com>
2023-01-29 19:18:20 +0300

    Add soln

--- python/leap.py
+++ python/leap.py
@@ -4,34 +4,14 @@ def leap_year(year):
     except every year that is evenly divisible by 100
     unless the year is also evenly divisible by 400
   """
-  mod_four = year // 4
-  mod_hund = year // 100
-  mod_four_hund = year // 400
-  print(mod_four)
-  print(mod_hund)
-  print(mod_four_hund)
-
-  if mod_four_hund % 2 == 0:
+  if year % 400 == 0:
     return True
-
-  # if mod_four % 2 == 0 and mod_hund % 2 == 0:
-  if mod_four % 2 == 0:
-    return True
-
-  if mod_hund % 2 != 0:
+  if year % 100 == 0:
     return False
-
+  if year % 4 == 0:
+    return True
   return False
 
-
-  # if mod_four % 2 == 0:
-  #   if mod_hund % 2 == 0:
-  #     if mod_four_hund % 2 == 0:
-  #       return True
-  #     return False
-  #   return True
-  # else:
-  #   return False
 
 print(leap_year(2100))
 print(leap_year(1996))
