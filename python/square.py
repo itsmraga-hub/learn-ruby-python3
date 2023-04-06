e268119450b7fbca0c508bf0a674147dc280432d
Raga <ragawilliam570@gmail.com>
2023-04-06 12:49:56 +0300

    Add position private attribute - a tuple

--- python/square.py
+++ python/square.py
@@ -1,5 +1,5 @@
 class Square:
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         # if type(size) == int and size < 0:
         #     raise ValueError('size must be >= 0')
         # elif type(size) == int:
@@ -7,11 +7,16 @@ class Square:
         # else:
         #     raise TypeError('size must be an integer')
         self.__size = size
+        self.__position = position
 
     # @size_getter
     @property
     def size(self):
         return self.__size
+
+    @property
+    def position(self):
+        return self.__position
 
     # @size_setter
     @size.setter
@@ -22,6 +27,13 @@ class Square:
             self.__size = size
         else:
             raise TypeError('size must be an integer')
+
+    @position.setter
+    def position(self, position):
+        if type(position) == tuple and position[0] > 0 and position[1] > 1:
+            self.__position = position
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         return self.__size * self.__size
