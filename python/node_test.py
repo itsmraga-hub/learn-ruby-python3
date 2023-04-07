674d160c5748eb10068cbfe43ec83c2fb15d9b54
Raga <ragawilliam570@gmail.com>
2023-04-07 21:06:53 +0300

    Test Node and SinglyLinkedList

---
+++ python/node_test.py
@@ -0,0 +1,17 @@
+from node import Node, SinglyLinkedList
+
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+print(sll.print_list())
+sll.sorted_insert(5)
+print(sll.print_list())
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
+# print(sll.print_list())
