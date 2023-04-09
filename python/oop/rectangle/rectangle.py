e0acdc4010e0b68fbce87655cf0f253fbb842c64
Raga <ragawilliam570@gmail.com>
2023-04-10 02:59:08 +0300

    Define rectangle class - task 0

--- python/oop/rectangle/rectangle.py
+++ python/oop/rectangle/rectangle.py
@@ -5,36 +5,5 @@
 
 class Rectangle:
     """A class defining a Rectangle"""
-    def __init__(self, width = 0, height = 0):
-        self.__width = width
-        self.__height = height
-
-
-    @property
-    def width(self):
-        return self.__width
-
-    @property
-    def height(self):
-        return self.__height
-
-    @width.setter
-    def width(self, width):
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
-
-    @height.setter
-    def height(self, height):
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = height
-
-        
+    pass
 
