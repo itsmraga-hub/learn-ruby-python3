e83495287601eb074ff49be678da333a9f0de4df
Raga <ragawilliam570@gmail.com>
2023-04-10 02:59:48 +0300

    Test task 1 setters and getters

--- python/oop/rectangle/rectangle_test.py
+++ python/oop/rectangle/rectangle_test.py
@@ -4,9 +4,9 @@ my_rectangle = Rectangle()
 print(type(my_rectangle))
 print(my_rectangle.__dict__)
 
-# my_rectangle = Rectangle(2, 4)
-# print(my_rectangle.__dict__)
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
 
-# my_rectangle.width = 10
-# my_rectangle.height = 3
-# print(my_rectangle.__dict__)
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
