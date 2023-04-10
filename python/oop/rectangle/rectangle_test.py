1f1976e903b019ddee65f1b8b896ae3d4385c1c7
Raga <ragawilliam570@gmail.com>
2023-04-10 19:58:51 +0300

    Test Rectangle.square classmethod

--- python/oop/rectangle/rectangle_test.py
+++ python/oop/rectangle/rectangle_test.py
@@ -122,18 +122,25 @@ from rectangle import Rectangle
 
 # Task 8
 
-my_rectangle_1 = Rectangle(8, 4)
-my_rectangle_2 = Rectangle(2, 3)
+# my_rectangle_1 = Rectangle(8, 4)
+# my_rectangle_2 = Rectangle(2, 3)
 
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
+# if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+#     print("my_rectangle_1 is bigger or equal to my_rectangle_2")
+# else:
+#     print("my_rectangle_2 is bigger than my_rectangle_1")
 
 
-my_rectangle_2.width = 10
-my_rectangle_2.height = 5
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
+# my_rectangle_2.width = 10
+# my_rectangle_2.height = 5
+# if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+#     print("my_rectangle_1 is bigger or equal to my_rectangle_2")
+# else:
+#     print("my_rectangle_2 is bigger than my_rectangle_1")
+
+
+# Task 9
+
+my_square = Rectangle.square(5)
+print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+print(my_square)
