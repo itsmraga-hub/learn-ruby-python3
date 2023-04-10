50075a52943a0776ef952882d8a6eef78b7bba72
Raga <ragawilliam570@gmail.com>
2023-04-10 12:23:36 +0300

    Test new eval and str and __repr__ method

--- python/oop/rectangle/rectangle_test.py
+++ python/oop/rectangle/rectangle_test.py
@@ -1,12 +1,69 @@
 from rectangle import Rectangle
 
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
+# Task 0
+
+# my_rectangle = Rectangle()
+# print(type(my_rectangle))
+# print(my_rectangle.__dict__)
+
+# Task 1
+
+# my_rectangle = Rectangle(2, 4)
+# print(my_rectangle.__dict__)
+
+# my_rectangle.width = 10
+# my_rectangle.height = 3
+# print(my_rectangle.__dict__)
+
+# Task 2
+
+# my_rectangle = Rectangle(2, 4)
+# print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+# print("--")
+
+# my_rectangle.width = 10
+# my_rectangle.height = 3
+# print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+# Task 3
+
+# my_rectangle = Rectangle(2, 4)
+# print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+# print(str(my_rectangle))
+# print(repr(my_rectangle))
+
+# print("--")
+
+# my_rectangle.width = 10
+# my_rectangle.height = 3
+# print(my_rectangle)
+# print(repr(my_rectangle))
+
+
+# Task 4
 
 my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
+print(str(my_rectangle))
+print("--")
+print(my_rectangle)
+print("--")
+print(repr(my_rectangle))
+print("--")
+print(hex(id(my_rectangle)))
+print("--")
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
+# create new instance based on representation
+new_rectangle = eval(repr(my_rectangle))
+print(str(new_rectangle))
+print("--")
+print(new_rectangle)
+print("--")
+print(repr(new_rectangle))
+print("--")
+print(hex(id(new_rectangle)))
+print("--")
+
+print(new_rectangle is my_rectangle)
+print(type(new_rectangle) is type(my_rectangle))
