35dd618c31f412e1e94e312d819e2d30ec9cf5e5
Raga <ragawilliam570@gmail.com>
2023-04-10 12:26:58 +0300

    Add test for Rectangle del method

--- python/oop/rectangle/rectangle_test.py
+++ python/oop/rectangle/rectangle_test.py
@@ -44,26 +44,37 @@ from rectangle import Rectangle
 
 # Task 4
 
-my_rectangle = Rectangle(2, 4)
-print(str(my_rectangle))
-print("--")
-print(my_rectangle)
-print("--")
-print(repr(my_rectangle))
-print("--")
-print(hex(id(my_rectangle)))
-print("--")
+# my_rectangle = Rectangle(2, 4)
+# print(str(my_rectangle))
+# print("--")
+# print(my_rectangle)
+# print("--")
+# print(repr(my_rectangle))
+# print("--")
+# print(hex(id(my_rectangle)))
+# print("--")
 
 # create new instance based on representation
-new_rectangle = eval(repr(my_rectangle))
-print(str(new_rectangle))
-print("--")
-print(new_rectangle)
-print("--")
-print(repr(new_rectangle))
-print("--")
-print(hex(id(new_rectangle)))
-print("--")
+# new_rectangle = eval(repr(my_rectangle))
+# print(str(new_rectangle))
+# print("--")
+# print(new_rectangle)
+# print("--")
+# print(repr(new_rectangle))
+# print("--")
+# print(hex(id(new_rectangle)))
+# print("--")
 
-print(new_rectangle is my_rectangle)
-print(type(new_rectangle) is type(my_rectangle))
+# print(new_rectangle is my_rectangle)
+# print(type(new_rectangle) is type(my_rectangle))
+
+# Task 5
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+del my_rectangle
+
+try:
+    print(my_rectangle)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
