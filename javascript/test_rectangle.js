20b305be0e5bda72079bffff7b5eb6893aa132f7
Raga <ragawilliam570@gmail.com>
2023-04-12 14:27:06 +0300

    Test for task 1

--- javascript/test_rectangle.js
+++ javascript/test_rectangle.js
@@ -1,5 +1,16 @@
-import Rectangle from './rectangle';
+import Rectangle from './rectangle.js';
 
-r = new Rectangle()
-console.log(r);
-console.log(r.constructor);
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
+
+const r2 = new Rectangle(2, -3);
+console.log(r2);
+console.log(r2.width);
+console.log(r2.height);
+
+const r3 = new Rectangle(2);
+console.log(r3);
+console.log(r3.width);
+console.log(r3.height);
