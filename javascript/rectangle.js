aba3de3819614f8aaff950c108742eee85d57b94
Raga <ragawilliam570@gmail.com>
2023-04-12 14:44:47 +0300

    Add task 3 - solution

--- javascript/rectangle.js
+++ javascript/rectangle.js
@@ -1,7 +1,19 @@
 export default class Rectangle {
   constructor(w, h) {
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let output = "";
+      for (let j = 0; j < this.width; j++) {
+        output += "X";
+      }
+      console.log(output);
+    }
   }
 }
 
