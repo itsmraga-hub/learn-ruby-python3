084d60d3f33ef93ac7ee8a4808b99b23fbe11c99
Raga <ragawilliam570@gmail.com>
2023-04-12 15:03:48 +0300

    Task 6 - work oncharPrint with argument method

--- javascript/square.js
+++ javascript/square.js
@@ -4,4 +4,23 @@ export default class Square extends Rectangle {
   constructor(size) {
     super(size, size);
   }
+
+  charPrint(c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      this.print(c);
+    }
+  }
+
+  print(c) {
+    // Function to print rectangle using X
+    for (let i = 0; i < this.height; i++) {
+      let output = "";
+      for (let j = 0; j < this.width; j++) {
+        output += c;
+      }
+      console.log(output);
+    }
+  }
 }
