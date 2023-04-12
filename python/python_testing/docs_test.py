3f22948a398ee27f0dec21002e6c5aba3380bc31
Raga <ragawilliam570@gmail.com>
2023-04-13 00:46:01 +0300

    Add docstest for add integer function

--- python/python_testing/docs_test.py
+++ python/python_testing/docs_test.py
@@ -1,5 +1,15 @@
 def add_integer(a, b=98):
-
+    """
+      >>> add_integer(8)
+      106
+      >>> add_integer(3, 6.0)       
+      9
+      >>> add_integer(3, '6.0') 
+      Traceback (most recent call last):
+        File "<stdin>", line 1, in <module>
+        File "<stdin>", line 6, in add_integer
+      TypeError: b must be an integer
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     
