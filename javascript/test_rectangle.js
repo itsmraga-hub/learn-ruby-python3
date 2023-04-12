09e0f01215922728ecf009fac4c5359c4451f27d
Raga <ragawilliam570@gmail.com>
2023-04-12 14:44:58 +0300

    Test Task 3 - print function

--- javascript/test_rectangle.js
+++ javascript/test_rectangle.js
@@ -1,16 +1,55 @@
 import Rectangle from './rectangle.js';
 
+//TODO: Task 0
+// const r1 = new Rectangle();
+// console.log(r1);
+// console.log(r1.constructor);
+
+
+//TODO: Task 1
+// const r1 = new Rectangle(2, 3);
+// console.log(r1);
+// console.log(r1.width);
+// console.log(r1.height);
+
+// const r2 = new Rectangle(2, -3);
+// console.log(r2);
+// console.log(r2.width);
+// console.log(r2.height);
+
+// const r3 = new Rectangle(2);
+// console.log(r3);
+// console.log(r3.width);
+// console.log(r3.height);
+
+
+// TODO: Task 2
+// const r1 = new Rectangle(2, 3);
+// console.log(r1);
+// console.log(r1.width);
+// console.log(r1.height);
+
+// const r2 = new Rectangle(2, -3);
+// console.log(r2);
+// console.log(r2.width);
+// console.log(r2.height);
+
+// const r3 = new Rectangle(2);
+// console.log(r3);
+// console.log(r3.width);
+// console.log(r3.height);
+
+// const r4 = new Rectangle(2, 0);
+// console.log(r4);
+// console.log(r4.width);
+// console.log(r4.height);
+
+// TODO: Task 3
 const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+r1.print();
 
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
+const r2 = new Rectangle(10, 5);
+r2.print();
 
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
+
+// TODO: Task 4
