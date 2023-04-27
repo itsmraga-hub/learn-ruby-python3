6f31d10b30bc870113710b4ba1b9e5efbb720304
Raga <ragawilliam570@gmail.com>
2023-04-27 15:55:58 +0300

    Save baby names to database

--- python/bincom/contact_session_3/baby_names_extract_save_to_db.py
+++ python/bincom/contact_session_3/baby_names_extract_save_to_db.py
@@ -1,20 +1,30 @@
 import re
+import psycopg2
 """"
     A name extract in python to read my name from a file and write the names to the terminal
 """
+
+# establishing the connection
+conn = psycopg2.connect(
+    database="todo_list", user='postgres', password='12345678', host='127.0.0.1', port='5432'
+)
+
+cursor = conn.cursor()
 
 path = 'python/bincom/contact_session_2/baby2008.html'
 # path = 'python/bincom/contact_session_3/file.html'
 
 try:
     with open(path, 'r') as f:
-        baby_file = f.read() # or f.read() because its only one name
+        baby_file = f.read()  # or f.read() because its only one name
         # pattern = r'<td>(.*?)</td>'
         pattern = r'<td>([A-Za-z]+)</td>'
         names = re.findall(pattern, baby_file)
         # match = re.search(pattern, baby_file)
-        for i in range(len(names)):
-            print("{}: {}".format(i, names[i]))
+        for name in names:
+            sql = f"INSERT INTO baby_names (name) VALUES ('{name}')"
+            cursor.execute(sql)
+            conn.commit()
 
         # print(baby_file)
 except FileNotFoundError:
