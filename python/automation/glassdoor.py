b6730b26c57a7c19f11d1f71568e5841afab3716
Raga <ragawilliam570@gmail.com>
2023-07-19 23:24:23 +0300

    Fetch glassdor jobs

--- python/automation/glassdoor.py
+++ python/automation/glassdoor.py
@@ -11,37 +11,42 @@ chrome_options = webdriver.ChromeOptions()
 driver = webdriver.Chrome('chromedriver', options=chrome_options)
 
 
+# url = 'https://www.glassdoor.com/Job/nigeria-jobs-SRCH_IL.0,7_IN177.htm'
+url = "https://www.glassdoor.com/Job/software-engineer-jobs-SRCH_KO0,17.htm"
 # driver.get("https://www.glassdoor.com/index.htm")
-driver.get('https://www.glassdoor.com/Job/index.htm')
+# driver.get('https://www.glassdoor.com/Job/index.htm')
+driver.get(url)
+
+jobs = driver.find_elements(By.CLASS_NAME, 'job-title')
+
+jobs_list = []
+for job in jobs:
+    jobs_list.append(job.text)
+
+print(jobs_list[:20])
+
 
 
 # job_title = driver.find_element("id", "typedKeyword-144bf2e-2611-2236-cdd8-e86c0a7de0fa") 
-job_title = driver.find_element(By.NAME, "typedKeyword")
+# job_title = driver.find_element(By.NAME, "typedKeyword")
 # print(job_title.__dict__)
 # print(dir(job_title))
-print(job_title.text)
+# print(job_title.text)
 
-job_title.send_keys('software engineer')
+# job_title.send_keys('software engineer')
 
-job_area = driver.find_element(By.NAME, "Autocomplete")
+# job_area = driver.find_element(By.NAME, "Autocomplete")
 # job_area = driver.find_element("id", "Autocomplete-575317-b7a2-c62-a3fb-2c4a153afef")
 
-job_title.send_keys("")
+# job_title.send_keys("")
 
-# gd-ui-button HeroSearch_searchButton__tg8_t css-24cilg e5tvpqr2
-# btn = driver.find_element("class", "HeroSearch_buttonTextOverflowOverride__xNE8E")
-# btn = driver.find_element(By.NAME, "clickSource")
-# print(dir(btn))
 
-btn = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "clickSource")))
-btn.click()
-# WebDriverWait(driver=driver, timeout=10).until(
-#     # lambda x: x.execute_script("return document.readyState === 'complete'")
-#     btn.click()
-# )
+# btn = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "clickSource")))
+# btn.click()
+
 
 # Wait for the page to redirect
-url = 'https://www.glassdoor.com/Job/nairobi-software-engineer-jobs-SRCH_IL.0,7_IC2896002_KO8,25.htm?suggestCount=0&suggestChosen=false&typedKeyword=software%2520engineer&dropdown=0&Autocomplete=Nairobi&clickSource=searchBtn'
+# url = 'https://www.glassdoor.com/Job/nairobi-software-engineer-jobs-SRCH_IL.0,7_IC2896002_KO8,25.htm?suggestCount=0&suggestChosen=false&typedKeyword=software%2520engineer&dropdown=0&Autocomplete=Nairobi&clickSource=searchBtn'
 
 try:
     WebDriverWait(driver, 10).until(EC.url_contains(url))
